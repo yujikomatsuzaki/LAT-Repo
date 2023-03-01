98600500d4cf1dcd1bdae089fc4c9e25bfb1be9f
Yuji <40947051s@ntnu.edu.tw>
2023-03-01 22:19:56 +0800

    work1

---
+++ hello.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv('foreign_students_by_department.csv')
+
+# print(df.to_string())
+
+df.head()
+df.info()
+
+df['學校名稱'].value_counts()
+
+temp = df[['學年度','學校名稱','外國學生小計']]
+temp
+
+Type = temp.groupby(['學校名稱'])['外國學生小計'].sum().reset_index()
+Type
+
+Type['Year'] = 2015
